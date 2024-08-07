813cd8306439d6a1bf6e45b30e8dd3ce417cd896
awslinuxadmin <scott.bakerphx@gmail.com>
2024-08-07 16:42:55 -0700

    Commit #3 for static data and local storage & load/delete.

--- appointment-app/src/app/appointment-list/appointment-list.component.ts
+++ appointment-app/src/app/appointment-list/appointment-list.component.ts
@@ -1,27 +1,54 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
-  styleUrls: ['./appointment-list.component.css']
+  styleUrls: ['./appointment-list.component.css'],
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit{
 
-  newAppointmentTitle : string = "";
+  
+  newAppointmentTitle: string = '';
+  
+  newAppointmentDate: Date = new Date();
+  
+  appointments: Appointment[] = [];
+  
+  ngOnInit(): void {
+    let savedAppointments = localStorage.getItem("appointments")
+    
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : []
+  }
 
-  newAppointmentDate  : Date = new Date();
+  /**
+   * How to push/add to an array
+   */
 
-  appointments: Appointment[] = []
-
-  addAppointment(){
-    if(this.newAppointmentTitle.trim().length && this.newAppointmentDate){
+  addAppointment() {
+    if (this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
       let newAppointment: Appointment = {
         id: Date.now(),
         title: this.newAppointmentTitle,
-        date: this.newAppointmentDate
-      }
+        date: this.newAppointmentDate,
+      };
+
+      this.appointments.push(newAppointment);
+
+      this.newAppointmentTitle = '';
+      this.newAppointmentDate = new Date();
+
+      localStorage.setItem('appointments', JSON.stringify(this.appointments)); // saves entire array locally
     }
   }
 
+  /**
+   * How to delete from an array
+   */
+
+  deleteAppointment(index: number) {
+    this.appointments.splice(index, 1);
+    localStorage.setItem('appointments', JSON.stringify(this.appointments));
+  }
 }
