14a1d862be0b72a59f2e42881179d483aa4710c2
awslinuxadmin <scott.bakerphx@gmail.com>
2024-08-06 18:06:11 -0700

    Commit for newly developing models and appopintment components.

--- appointment-app/src/app/appointment-list/appointment-list.component.ts
+++ appointment-app/src/app/appointment-list/appointment-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
 
 @Component({
   selector: 'app-appointment-list',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./appointment-list.component.css']
 })
 export class AppointmentListComponent {
+
+  newAppointmentTitle : string = "";
+
+  newAppointmentDate  : Date = new Date();
+
+  appointments: Appointment[] = []
+
+  addAppointment(){
+    if(this.newAppointmentTitle.trim().length && this.newAppointmentDate){
+      let newAppointment: Appointment = {
+        id: Date.now(),
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate
+      }
+    }
+  }
 
 }
